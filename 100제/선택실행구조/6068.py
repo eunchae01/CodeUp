0755883300d3f3ea0da2e46325ffc85e59a961dd
eunchae <eunchae01@naver.com>
2023-02-03 09:45:51 +0900

    선택실행구조

---
+++ 100제/선택실행구조/6068.py
@@ -0,0 +1,16 @@
+# 입력
+# 정수(0 ~ 100) 1개가 입력된다.
+
+# 출력
+# 평가 결과를 출력한다.
+
+num = int(input())
+
+if(num >= 90):
+    print("A")
+elif(num >= 70):
+    print("B")
+elif(num >= 40):
+    print("C")
+else:
+    print("D")
---
+++ 100제/선택실행구조/6069.py
@@ -0,0 +1,19 @@
+# 입력
+# 영문자 1개가 입력된다.
+# (A, B, C, D 등 문자 1개가 입력된다.)
+
+# 출력
+# 문자에 따라 다른 내용이 출력된다.
+
+word = input()
+
+if(word == "A"):
+    print("best!!!")
+elif(word == "B"):
+    print("good!!")
+elif(word == "C"):
+    print("run!")
+elif(word == "D"):
+    print("slowly~")
+else:
+    print("what?")
